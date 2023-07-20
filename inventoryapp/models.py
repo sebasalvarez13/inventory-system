9a92dfe29c34ab1b22d92930dcb0f14a5e032240
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2023-07-20 11:25:14 -0700

    Update class name to Products

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -2,7 +2,7 @@
 
 from . import db
 
-class Product(db.Model):
+class Products(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     price = db.Column(db.Float)
