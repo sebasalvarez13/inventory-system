da3a15e469c3b463e10ef4f672e43907335914bb
sebasalvarez13 <sebastian.alvarezarce@gmail.com>
2023-07-20 16:32:18 -0700

    Add vendor_id filed to class Products

--- inventoryapp/models.py
+++ inventoryapp/models.py
@@ -7,8 +7,9 @@ class Products(db.Model):
     name = db.Column(db.String(100))
     price = db.Column(db.Float)
     stock = db.Column(db.Integer)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'))
 
-class Vendors(db.model):
+class Vendors(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     country = db.Column(db.String(100))
